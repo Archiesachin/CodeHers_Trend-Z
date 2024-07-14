0983d7838f4a158585ed220202ca1b4b9982e3f6
Archiesachin <archieshah8767@gmail.com>
2024-07-15 00:18:18 +0530

    Profile section added

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import {icons} from '../../constants'
+import { useNavigation } from "@react-navigation/native";
+
+
 
 const profile = () => {
+  const navigation = useNavigation();
   return (
     <View>
-      <Text>profile</Text>
+    <View className="bg-secondary-100 h-[300px] mt-10" >
+    <View className="flex-row mt-10  justify-between items-center">
+      <TouchableOpacity onPress={() => navigation.goBack()} >
+        <AntDesign name="arrowleft" size={24} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <AntDesign name="setting" size={35} color="white" />
+      </TouchableOpacity>
+      </View>
+    </View>
+    <View className="bg-white rounded-2xl">
+    <Image
+      source={icons.profile}
+      resizeMode="contain"
+      className="w[50px] h[50px] rounded-[50%]"
+    />
+
+    </View>
     </View>
   )
 }
+
 
 export default profile
